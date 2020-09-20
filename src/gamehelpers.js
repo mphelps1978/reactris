baf31a0ceed1264875a692e1721889d13c017492
Michael Phelps <michaeljphelps1978@gmail.com>
2020-09-20 13:28:44 -0500

    :sparkles: Collisions, Styles
    Creates collision logic, and styled components for application

--- src/gamehelpers.js
+++ src/gamehelpers.js
@@ -5,3 +5,27 @@ export const createStage = () =>
 	Array.from(Array(STAGE_HEIGHT), () =>
 		new Array(STAGE_WIDTH).fill([0, 'clear'])
 	)
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+	// for loops needed here to be able to return and break
+	for (let y = 0; y < player.tetromino.length; y += 1) {
+		for (let x = 0; (x = player.tetromino[y].length); x += 1) {
+			if (player.tetromino[y][x] !== 0) {
+				// if we're on an actual Tetromino cell
+				if (
+					// make sure move is inside the game area height (y)
+					// and we aren't going through the bottom of the game area
+					!stage[y + player.pos.y + moveY] ||
+					// make sure we're within the game are width (x)
+					!stage[y + player.pos.y + moveY][(x + player.pos.x) | moveX] ||
+					// and finally make sure we aren't in a cell marked clear
+					stage[y + player.pos.y + moveY][(x + player.pos.x) | moveX][1] !==
+						'clear'
+				) {
+					return true // We're colliding with something
+				}
+			}
+		}
+	}
+	return false // All clear, let's move the peice
+}
